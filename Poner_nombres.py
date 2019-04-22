1268b883e295823193609636856866a1928eddb2
Juan Carlos Saravia Drago <jsaraviadrago@gmail.com>
2019-04-22 15:40:44 -0500

    Add files via upload

---
+++ Poner_nombres.py
@@ -0,0 +1,13 @@
+file = open("Nombres_variables.txt", "w+")
+lista = []
+print('Hola! Vamos a nombrar cosas')
+
+x = input('Que cosa quieres nombrar\n')
+y = input('Con que lo quieres separar\n')
+n = input('Cantidad de filas')
+n = int(n)
+
+for i in range(1,n):
+    z = (x + str(i).ljust(1) + y)
+    file.write(z)
+file.close()
