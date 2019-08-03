61784facacaac838b3dd418357d2367907b55f4b
Juan Carlos Saravia <jsaraviadrago@gmail.com>
2019-08-03 17:50:43 -0500

    Armando calculadora

--- Calculadora.py
+++ Calculadora.py
@@ -1,2 +1,19 @@
 plata = input('Poner aca el importe mensual\n')
-print(plata)
+plata = float(plata)
+mes = float(12)
+gratificacion = float(2)
+bono = input('Cuantos bonos tienes\n')
+bono = float(bono)
+
+segplata = input('Poner la cantidad de trabajos adicionales\n')
+segplata = float(segplata)
+
+if segplata != 0:
+    platados = input('Poner segundo importe mensual\n')
+    platados = float(platados)
+    respuesta = ((plata+platados)*(mes+bono+gratificacion))/14
+else:
+   respuesta = (plata*(mes+bono+gratificacion))/14
+
+print(respuesta)
+
