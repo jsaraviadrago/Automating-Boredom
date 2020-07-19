b71f66a625d08baa7d607f55f6ab1cd9b7aca5f8
jcsd84 <jcsd84.jc@gmail.com>
2020-07-18 20:33:54 -0500

    Update

--- Poner_nombres.py
+++ Poner_nombres.py
@@ -1,13 +1,37 @@
-file = open("Nombres_variables.txt", "w+")
+import os.path as os
+
+save_path = '/Users/home/Downloads'
+
+file = "Nombres_variables"
+
+file_name_root = os.join(save_path, file+".txt")
+
+file_final = open(file_name_root, "w+")
+
 lista = []
+
 print('Hola! Vamos a nombrar cosas')
 
 x = input('Que cosa quieres nombrar\n')
+if x == '':
+    x = 'Item'
+else:
+    x = x
 y = input('Con que lo quieres separar\n')
+if y == '':
+    y = '+'
+else:
+    y = y
+
 n = input('Cantidad de filas\n')
+if n == '':
+    n = '30'
+else:
+    n = n
 n = int(n)
 
 for i in range(1,n):
     z = (x + str(i).ljust(1) + y)
-    file.write(z)
-file.close()
+    file_final.write(z)
+file_final.close()
+
