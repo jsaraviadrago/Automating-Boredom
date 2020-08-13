b89e4386a0a11402235244a669e1841225d762dd
jcsd84 <jsaraviadrago@gmail.com>
2020-08-13 18:56:18 -0500

    Update

--- Scraping_web_pages.py
+++ Scraping_web_pages.py
@@ -2,21 +2,21 @@ from lxml import html
 import requests
 import pandas as pd
 
-page = requests.get('http://econpy.pythonanywhere.com/ex/001.html')
-tree = html.fromstring(page.content)
+page = requests.get('https://es.wikipedia.org/wiki/Clasificación_de_Conmebol_para_la_Copa_Mundial_de_Fútbol_de_2018')
+#tree = html.fromstring(page.content)
+print(page.content)
 
-buyers = tree.xpath('//div[@title="buyer-name"]/text()')
-prices = tree.xpath('//span[@class="item-price"]/text()')
+#buyers = tree.xpath('//div[@title="buyer-name"]/text()')
+#prices = tree.xpath('//span[@class="item-price"]/text()')
 
-a = {'Buyers': buyers}
-b = {'Prices': prices}
-d = pd.DataFrame({**a,**b})
+#a = {'Buyers': buyers}
+#b = {'Prices': prices}
+#d = pd.DataFrame({**a,**b})
 
-d["Prices"] = d["Prices"].str.replace('$', '')
-d["Prices"] = pd.to_numeric(d["Prices"])
+#d["Prices"] = d["Prices"].str.replace('$', '')
+#d["Prices"] = pd.to_numeric(d["Prices"])
 #promedio = d["Prices"].mean()
 #print(d)
-#print(promedio)
 
 
 
