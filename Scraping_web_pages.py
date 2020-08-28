2a459766428b1a4013c65b8a29e924977677a6e3
jcsd84 <jsaraviadrago@gmail.com>
2020-08-28 08:56:32 -0500

    Update

--- Scraping_web_pages.py
+++ Scraping_web_pages.py
@@ -1,22 +1,17 @@
-from lxml import html
-import requests
-import pandas as pd
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import ssl
 
-page = requests.get('https://es.wikipedia.org/wiki/Clasificación_de_Conmebol_para_la_Copa_Mundial_de_Fútbol_de_2018')
-#tree = html.fromstring(page.content)
-print(page.content)
+ssl._create_default_https_context = ssl._create_unverified_context
 
-#buyers = tree.xpath('//div[@title="buyer-name"]/text()')
-#prices = tree.xpath('//span[@class="item-price"]/text()')
+html = urlopen('https://en.wikipedia.org/wiki/2022_FIFA_World_Cup_qualification_(CONMEBOL)')
+bs = BeautifulSoup(html.read(), 'lxml')
 
-#a = {'Buyers': buyers}
-#b = {'Prices': prices}
-#d = pd.DataFrame({**a,**b})
+nameList = bs.findAll('span', {'class':'green'})
+for name in nameList:
+    print(name.get_text())
 
-#d["Prices"] = d["Prices"].str.replace('$', '')
-#d["Prices"] = pd.to_numeric(d["Prices"])
-#promedio = d["Prices"].mean()
-#print(d)
+prueba = bs.find_all(['h1','h2','h3','h4'])
 
 
 
