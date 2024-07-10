b9ed2a2fed8dd857fe586278cb169fba28303054
JC Saravia <jsaraviadrago@gmail.com>
2024-07-10 15:26:56 -0500

    Editing a table of garmin runs

---
+++ Script_teams_peru.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+# This scrapper was made to retrieve all teams in peru from Wikiepdia.
+
+url = "https://es.wikipedia.org/wiki/Anexo:Clubes_de_fútbol_del_Perú"
+
+dfs = []
+soup = BeautifulSoup(requests.get(url).content, "html.parser")
+
+tables = soup.find_all(
+    lambda tag: tag.name == "table"
+        and tag.select_one('th:-soup-contains("Equipo", "Ciudad", "Fundación", "Estadio", "Liga")'))
+
+for table in tables:
+    for tag in table.select('[style="display:none"]'):
+        tag.extract()
+    df = pd.read_html(str(table))[0]
+    df['Region'] = table.find_previous(["h3","h2"]).span.text
+    dfs.append(df)
+
+df = pd.concat(dfs)
+print(dfs)
+
+
+#df.to_csv("/Users/home/Downloads/data_results_manager.csv", index=False)
+
