6ac6bef0344d8acd431459e6ee39c84ef5c58dde
JC Saravia <jsaraviadrago@gmail.com>
2024-07-11 10:32:39 -0500

    Editing a table of garmin runs

--- Script_teams_peru.py
+++ Script_teams_peru.py
@@ -1,29 +1,24 @@
-import requests
 import pandas as pd
-from bs4 import BeautifulSoup
-from io import StringIO
+from urllib.parse import quote
 
-# This scrapper was made to retrieve all teams in peru from Wikiepdia.
-
+# URL de la página
 url = "https://es.wikipedia.org/wiki/Anexo:Clubes_de_fútbol_del_Perú"
 
-dfs = []
-soup = BeautifulSoup(requests.get(url).content, "html.parser")
+# Codificar la URL
+url_codificada = quote(url, safe=':/')
 
-tables = soup.find_all(
-    lambda tag: tag.name == "table"
-        and tag.select_one('th:-soup-contains("Equipo", "Ciudad", "Fundación", "Estadio", "Liga")'))
+try:
+    tablas = pd.read_html(url_codificada)
+except Exception as e:
+    print(f"Ocurrió un error: {e}")
 
-for table in tables:
-    for tag in table.select('[style="display:none"]'):
-        tag.extract()
-    df = pd.read_html(StringIO(str(table)))[0]
-    df['Region'] = table.find_previous(["h3","h2"]).span.text
-    dfs.append(df)
-
-df = pd.concat(dfs)
-print(dfs)
-
-
-#df.to_csv("/Users/home/Downloads/data_results_manager.csv", index=False)
-
+clean_tables :list[pd.DataFrame] = []
+for table in tablas:
+    tag = list(table.columns)[0]
+    if tag not in ('Equipo', 'Club'):
+        region = tag
+    if any(item in ['Equipo', 'Ciudad', 'Fundación', 'Estadio', 'Liga'] for item in list(table.columns)):
+        print(f'Exportando tabla: {region}')
+        table['region'] = region
+        clean_tables.append(table)
+        table.to_csv(f"C:\\Users\\JuanCarlosSaraviaDra\\Downloads\\table_{region}.csv", index=False)
